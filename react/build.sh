53a0009f869afbaf915128e6b5c0381a24a55dc8
02loveslollipop <mmonsalve2005@gmail.com>
2025-03-02 20:44:02 -0500

    feat: migrate to Capacitor Android, add necessary configurations and resources

--- react/build.sh
+++ react/build.sh
@@ -2,6 +2,8 @@
 # This script builds the React app and compiles a Ionic hybrid app
 
 npm run build # Build the React app
+cp -r dist ../capacitor-android/dist # Copy the build to the Capacitor android project
+cp -r dist ../capacitor-ios/dist # Copy the build to the Capacitor ios project
 npx cap sync # Sync the Capacitor project
 npx cap android # Build the Android app
 echo "Build complete"
