6cf576384c21612df1ca0949015d8b75cfe82ad3
02loveslollipop <mmonsalve2005@gmail.com>
2025-03-06 22:24:29 -0500

    feat: update site URL to new Vercel deployment

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,7 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cuantopal3web.02loveslollipop.uk',
+  site: 'https://cuantopal3-web.vercel.app/',
   compressHTML: true,
   integrations: [icon()],
   server : {
